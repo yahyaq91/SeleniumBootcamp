0082d8ece7f21fb08d5ff0395bdb13a263a87bda
yahyaq91 <yahyaq91@gmail.com>
2022-05-05 22:06:36 -0400

    added test cases to market watch for the US market, added test steps and tst data along with setting up TestNGRunner to run test cases for USmarket tests.

--- com.marketwatch/src/main/java/shared/Homepage.java
+++ com.marketwatch/src/main/java/shared/Homepage.java
@@ -1,11 +1,28 @@
 package shared;
 
 import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import pom.UnitedStatesMarket;
 
 public class Homepage extends BasePage {
 
+    @FindBy(xpath = "//a[@href='https://www.marketwatch.com/markets/us']")
+    public WebElement usMarket;
+
+    @FindBy(xpath = "//button[@class='close-btn']")
+    public WebElement popUP;
+
+    @FindBy(xpath = "//h1[@class='company__name']")
+    public WebElement dataSelectionConfirmation;
+
     public Homepage(){
         PageFactory.initElements(driver, this);
+    }
+    public UnitedStatesMarket closePopUp(){
+        clickOnElement(popUP);
+
+        return new UnitedStatesMarket();
     }
 }
